f1f03277be8fcc4e6640a854e19b89b3aeea91b2
trident2710 <andriydychka@gmail.ocm>
2025-03-22 11:31:52 +0200

    Cli runner initial impl

---
+++ cli-runner/src/main/java/ua/kpi/softeng_course/tictactoe/runner/Main.java
@@ -0,0 +1,36 @@
+package ua.kpi.softeng_course.tictactoe.runner;
+
+import ua.kpi.softeng_course.tictactoe.model.TicTacToeRoundModelImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello tictactoe!");
+        System.out.println("Wanna start round?");
+        var answer = System.console().readLine();
+
+        if (answer.equalsIgnoreCase("yes")) {
+            var model = new TicTacToeRoundModelImpl();
+
+            while (model.nextAction().isPresent()) {
+                var nextAction = model.nextAction().get();
+                var action = nextAction.getAction();
+                switch (action) {
+                    case START -> {
+                        System.out.println("Starting?");
+                        if (System.console().readLine().equals("yes")) {
+                            model.start();
+                        }
+                    }
+                    case FINISH -> {
+                        System.out.println("Finishing?");
+                        if (System.console().readLine().equals("yes")) {
+                            model.finish();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
