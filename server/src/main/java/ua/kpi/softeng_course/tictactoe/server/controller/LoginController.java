1b1e11fe88c7c5595820a76f9b8db296e8cb7286
trident2710 <andriydychka@gmail.ocm>
2025-05-23 00:07:59 +0300

    Add structured logging

--- server/src/main/java/ua/kpi/softeng_course/tictactoe/server/controller/LoginController.java
+++ server/src/main/java/ua/kpi/softeng_course/tictactoe/server/controller/LoginController.java
@@ -1,5 +1,7 @@
 package ua.kpi.softeng_course.tictactoe.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +13,11 @@ import ua.kpi.softeng_course.tictactoe.server.api.LoginResponse;
 import ua.kpi.softeng_course.tictactoe.server.service.UserService;
 import ua.kpi.softeng_course.tictactoe.server.store.SessionStore;
 
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
 @RestController
 public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     private final SessionStore sessionStore;
     private final UserService userService;
 
@@ -24,14 +29,26 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-        System.out.println(loginRequest);
+        logger.info("Login attempt",
+                keyValue("username", loginRequest.username()),
+                keyValue("timestamp", System.currentTimeMillis()));
 
         var user = userService.findByUsername(loginRequest.username());
         if (user.isEmpty()) {
+            logger.warn("Login failed - user not found",
+                    keyValue("username", loginRequest.username()),
+                    keyValue("timestamp", System.currentTimeMillis()));
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
         String sessionId = sessionStore.createSession(user.get().id());
+
+        logger.info("User successfully logged in",
+                keyValue("username", user.get().username()),
+                keyValue("userId", user.get().id()),
+                keyValue("sessionId", sessionId),
+                keyValue("timestamp", System.currentTimeMillis()));
+
         return ResponseEntity.ok(new LoginResponse(user.get(), sessionId));
     }
 }
